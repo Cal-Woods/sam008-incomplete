d78cbdab53b8cb160b7f9d72241fa6eb9a630bcc
Cal Woods <D00270955@student.dkit.ie>
2025-02-19 12:51:02 +0000

    Added 'circleStore' class.

---
+++ utils/circle/circleStore.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class circleStore {
+    //Attributes
+    private ArrayList<Circle> hoops = new ArrayList<Circle>();
+
+    
+    
+    //Manipulator methods
+    public void addCircle(Circle circle) {
+        
+    }
+}
