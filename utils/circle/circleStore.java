23eb0574dc2cf6cbc08faccb71e5dbd0d80b6f8a
Cal Woods <D00270955@student.dkit.ie>
2025-02-21 11:45:11 +0000

    Added 'addCircle()' & 'showRadii()' methods.

--- utils/circle/circleStore.java
+++ utils/circle/circleStore.java
@@ -7,7 +7,22 @@ public class circleStore {
     
     
     //Manipulator methods
+    /**
+     * Adds a Circle object to a list.
+     * @param circle A Circle object or a child of Circle
+     */
     public void addCircle(Circle circle) {
-        
+        //Call 'add()' ArrayList method to add circle
+        hoops.add(circle);
     }
+
+    public void showRadii() {
+        //Initialise for loop
+        for (int i = 0; i < hoops.size(); i++) {
+            //Show each Circle radius
+            System.out.println(hoops.get(i).getRadius()+"\n");
+        }
+    }
+
+    
 }
